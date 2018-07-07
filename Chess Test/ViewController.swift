3c04174d184d6aeec1df7cd75412d7dca60d2bbf
Alush Benitez <alushb5@yahoo.com>
2018-07-07 11:16:05 -0500

    Checker pieces set up in starting positions

--- Chess Test/ViewController.swift
+++ Chess Test/ViewController.swift
@@ -12,10 +12,28 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     @IBOutlet weak var chessboardCollectionView: UICollectionView!
     
+    let blueStart = [1, 3, 5, 7, 8, 10, 12, 14, 17, 19, 21, 23]
+    let redStart = [41, 43, 45, 47, 48, 50, 52, 54, 57, 59, 61, 63]
+    
+    var whiteSquares: [Int] = []
+    var blackSquares: [Int] = []
+
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         chessboardCollectionView.dataSource = self
         chessboardCollectionView.delegate = self
+        
+        for num in 0 ... 63 {
+            if num % 2 == 1 {
+                blackSquares.append(num)
+            } else {
+                whiteSquares.append(num)
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,7 +50,16 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         //Whats gonna be in each cell
         let cell = chessboardCollectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CheckerSquare
         
-        cell.cellImage.image = UIImage(named: "clearness")
+        if blueStart.contains(count){
+            cell.cellImage.image = UIImage(named: "blue-checker")
+        } else if redStart.contains(count){
+            cell.cellImage.image = UIImage(named: "Red_checker")
+        } else {
+            cell.cellImage.image = UIImage(named: "clear-image")
+        }
+        
+        count += 1
+        
         return cell
         
     }
