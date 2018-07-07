d96fe0d0df5e3087edb390feae69e24301fc6597
Alush Benitez <alushb5@yahoo.com>
2018-07-07 17:46:20 -0500

    Green Squares set up to be in right spots

--- Chess Test/CheckerSquare.swift
+++ Chess Test/CheckerSquare.swift
@@ -7,17 +7,32 @@
 //
 
 import UIKit
-
+/*
 class CheckerSquare: UICollectionViewCell {
     @IBOutlet weak var cellImage: UIImageView!
     
     var isOccupied = false
+    var color = ""
+    var pieceToMove: CheckerSquare? = nil
+    var position = -1
+    let allSquares = getCheckerSquaresArray()
     
     override var isSelected: Bool{
         didSet{
             if isOccupied {
                 if self.isSelected {
                     self.contentView.backgroundColor = UIColor.gray
+                    var movementOptions: [Int] = []
+                    
+                    if color == "red" {
+                        movementOptions = findAvalibleBottomSquares(position: position)
+                    } else {
+                        movementOptions = findAvalibleTopSquares(position: position)
+                        movementOptions += findAvalibleBottomSquares(position: position)
+                    }
+                    for cell in movementOptions {
+                        cell.contentView.backgroundColor = UIColor.green
+                    }
                 } else {
                     self.contentView.backgroundColor = UIColor.black
                 }
@@ -25,4 +40,45 @@ class CheckerSquare: UICollectionViewCell {
         }
     }
     
+    func findAvalibleTopSquares(position: Int) -> [Int]{
+        let row = position / 8
+        let firstInRow = row * 8
+        let lastInRow = ((row + 1) * 8) - 1
+        
+        let bottomRight = position + 9
+        let bottomLeft = position + 7
+        
+        var avalible: [Int] = []
+        
+        if !(bottomLeft > 63 || bottomLeft < firstInRow) {
+            avalible.append(bottomLeft)
+        }
+        if bottomRight > 63 || bottomRight > lastInRow {
+            avalible.append(bottomRight)
+        }
+        
+        return avalible
+    }
+    
+    func findAvalibleBottomSquares(position: Int) -> [Int]{
+        let row = position / 8
+        let firstInRow = row * 8
+        let lastInRow = ((row + 1) * 8) - 1
+        
+        let bottomRight = position + 9
+        let bottomLeft = position + 7
+        
+        var avalible: [Int] = []
+        
+        if !(bottomLeft > 63 || bottomLeft < firstInRow) {
+            avalible.append(bottomLeft)
+        }
+        if bottomRight > 63 || bottomRight > lastInRow {
+            avalible.append(bottomRight)
+        }
+        
+        return avalible
+    }
+    
 }
+*/
--- Chess Test/ViewController.swift
+++ Chess Test/ViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+var checkerSquares: [CheckerSquare] = []
+
+
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet weak var chessboardCollectionView: UICollectionView!
@@ -17,11 +20,9 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     var whiteSquares: [Int] = []
     var blackSquares: [Int] = []
     
-    var checkerSquares: [CheckerSquare] = []
     var occupiedSquares: [CheckerSquare] = []
     
     var blueTurn = true
-
     
     var count = 0
     
@@ -29,7 +30,6 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         super.viewDidLoad()
         chessboardCollectionView.dataSource = self
         chessboardCollectionView.delegate = self
-        //chessboardCollectionView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tap)))
         
         for num in 0 ... 63 {
             if num % 2 == 1 {
@@ -43,7 +43,6 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     
@@ -59,19 +58,102 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             cell.cellImage.image = UIImage(named: "blue-checker")
             occupiedSquares.append(cell)
             cell.isOccupied = true
+            cell.color = "blue"
         } else if houndStart.contains(count){
             cell.cellImage.image = UIImage(named: "Red_checker")
             cell.isOccupied = true
             occupiedSquares.append(cell)
+            cell.color = "red"
         } else {
             cell.cellImage.image = UIImage(named: "clear-image")
         }
         
         checkerSquares.append(cell)
         
+        cell.position = count
         count += 1
         
         return cell
     }
+}
+
+
+
+
+
+
+class CheckerSquare: UICollectionViewCell {
+    @IBOutlet weak var cellImage: UIImageView!
+    
+    var isOccupied = false
+    var position = -1
+    var color = ""
+    
+    override var isSelected: Bool{
+        didSet{
+            if isOccupied {
+                if self.isSelected {
+                    self.contentView.backgroundColor = UIColor.gray
+                    var movementOptions: [CheckerSquare] = []
+                    
+                    if self.color == "red" {
+                        movementOptions = findAvalibleBottomSquares(position: self.position)
+                    } else {
+                        movementOptions = findAvalibleTopSquares(position: self.position)
+                        movementOptions += findAvalibleBottomSquares(position: self.position)
+                    }
+                    print(self.color)
+                    for cell in movementOptions {
+                        cell.contentView.backgroundColor = UIColor.green
+                        print(cell.position)
+                    }
+                    
+                } else {
+                    self.contentView.backgroundColor = UIColor.black
+                }
+            }
+        }
+    }
+    
+    func findAvalibleTopSquares(position: Int) -> [CheckerSquare]{
+        let row = (position + 8) / 8
+        let firstInRow = (row * 8)
+        let lastInRow = ((row + 1) * 8) - 1
+        
+        let topRight = position - 7
+        let topLeft = position - 9
+        
+        var avalible: [CheckerSquare] = []
+        
+        if !(topLeft < 0 || topLeft < firstInRow) {
+            avalible.append(checkerSquares[topLeft])
+        }
+        if !(topRight < 0 || topRight > lastInRow) {
+            avalible.append(checkerSquares[topRight])
+        }
+        
+        return avalible
+    }
+    
+    func findAvalibleBottomSquares(position: Int) -> [CheckerSquare]{
+        let row = (position + 8) / 8
+        let firstInRow = (row * 8)
+        let lastInRow = ((row + 1) * 8) - 1
+        
+        let bottomRight = position + 9
+        let bottomLeft = position + 7
+        
+        var avalible: [CheckerSquare] = []
+        
+        if !(bottomLeft > 63 || bottomLeft < firstInRow) {
+            avalible.append(checkerSquares[bottomLeft])
+        }
+        if !(bottomRight > 63 || bottomRight > lastInRow) {
+            avalible.append(checkerSquares[bottomRight])
+        }
+        
+        return avalible
+    }
+    
 }
 
